42a19976bbfe4f8b2970fe834ca2ad7b1b93c2f8
Paulo Almeida <estpaulovictora@live.com>
2021-06-23 19:28:03 -0300

    First commit

---
+++ src/test/java/br/com/grokhong/sbmongo/SbmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.grokhong.sbmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
