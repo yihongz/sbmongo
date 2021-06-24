c0e6637633d4541f92efda650dcb504eee699005
Paulo Almeida <estpaulovictora@live.com>
2021-06-23 23:33:47 -0300

    Inserting user using DTO

--- src/main/java/br/com/grokhong/sbmongo/services/UserService.java
+++ src/main/java/br/com/grokhong/sbmongo/services/UserService.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import br.com.grokhong.sbmongo.dto.UserDTO;
 import br.com.grokhong.sbmongo.entities.User;
 import br.com.grokhong.sbmongo.repositories.UserRepository;
 import br.com.grokhong.sbmongo.services.exceptions.ObjectNotFoundException;
@@ -23,5 +24,13 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> user = Optional.ofNullable(repo.findById(id).orElse(null));
 		return user.orElseThrow(() -> new ObjectNotFoundException(id));
+	}
+	
+	public User insert(User obj) {
+		return repo.save(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 }
