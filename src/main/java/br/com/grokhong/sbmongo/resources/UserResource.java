52b5e49bdc6575d937dccb31912afee2f258ca4f
Paulo Almeida <estpaulovictora@live.com>
2021-06-23 22:50:03 -0300

    Using DTO pattern to return users

--- src/main/java/br/com/grokhong/sbmongo/resources/UserResource.java
+++ src/main/java/br/com/grokhong/sbmongo/resources/UserResource.java
@@ -1,8 +1,7 @@
 package br.com.grokhong.sbmongo.resources;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.grokhong.sbmongo.domain.User;
+import br.com.grokhong.sbmongo.dto.UserDTO;
 import br.com.grokhong.sbmongo.services.UserService;
 
 @RestController
@@ -21,8 +21,9 @@ public class UserResource {
 	private UserService service;
 	
 	@GetMapping
-	public ResponseEntity<List<User>> findAll() {
+	public ResponseEntity<List<UserDTO>> findAll() {
 		List<User> list = service.findAll();
-		return ResponseEntity.ok().body(list);
+		List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
 	}
 }
