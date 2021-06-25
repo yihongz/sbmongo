8ff6383804a34b2b1e111c5edd11ef6a467b88d2
Paulo Almeida <estpaulovictora@live.com>
2021-06-24 21:36:53 -0300

    Endpoint to return a user's posts

--- src/main/java/br/com/grokhong/sbmongo/resources/UserResource.java
+++ src/main/java/br/com/grokhong/sbmongo/resources/UserResource.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.grokhong.sbmongo.dto.UserDTO;
+import br.com.grokhong.sbmongo.entities.Post;
 import br.com.grokhong.sbmongo.entities.User;
 import br.com.grokhong.sbmongo.services.UserService;
 
@@ -60,5 +61,11 @@ public class UserResource {
 		obj.setId(id);
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@GetMapping(value = "/{id}/posts")
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
 	}
 }
