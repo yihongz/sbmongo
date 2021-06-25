f350ef49fc86b14661c57a8d8641e35f5ed2e5c4
Paulo Almeida <estpaulovictora@live.com>
2021-06-24 22:20:05 -0300

    Testing @Query

--- src/main/java/br/com/grokhong/sbmongo/repositories/PostRepository.java
+++ src/main/java/br/com/grokhong/sbmongo/repositories/PostRepository.java
@@ -3,6 +3,7 @@ package br.com.grokhong.sbmongo.repositories;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import br.com.grokhong.sbmongo.entities.Post;
@@ -10,5 +11,8 @@ import br.com.grokhong.sbmongo.entities.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
 
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
+	
 	List<Post> findByTitleContainingIgnoreCase(String text);
 }
